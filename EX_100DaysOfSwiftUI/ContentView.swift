0da8f8746cae14667fd5fa8b21b90dc81640abe0
doyeomKim <whqtkf12@naver.com>
2022-11-12 23:22:12 +0900

    Initial Commit

---
+++ EX_100DaysOfSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EX_100DaysOfSwiftUI
+//
+//  Created by 김도연 on 2022/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EX_100DaysOfSwiftUI/EX_100DaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EX_100DaysOfSwiftUIApp.swift
+//  EX_100DaysOfSwiftUI
+//
+//  Created by 김도연 on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct EX_100DaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
